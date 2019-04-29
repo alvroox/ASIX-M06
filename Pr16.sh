3ed4a1255f1bd0f697aa1d0220b57188765d21f5
davmelvil <47982409+davmelvil@users.noreply.github.com>
2019-04-29 18:13:46 +0200

    Add files via upload

--- Pr16.sh
+++ Pr16.sh
@@ -18,46 +18,27 @@ exit 1
 fi
 
 echo -e "\n"
-echo "Ingrese el nombre completo:" #Introducir nombre de usuario en la descripción.
-read nombrec
-if [ "$nombrec" == "" ] ; then
-clear
-echo -e "\n"
-echo "El nombre no puede estar vacío"
-echo -e "\n"
-exit 1
-fi
+echo "Numero de telefono  : "
+read comment
+echo "Dirección  : "
+read comment2
 
-echo -e "\n"
-echo "Ingrese el home directory (default /home/$nombre): " #Crear directorio para el usuario
-read home
-if [ "$home" == "" ] ; then
-home="/home/$nombre"
-fi
-
-echo -e "\n"
-read -s -p "Ingrese la contraseña del usuario : " password
-if [ "$password" == "" ] ; then
-#clear
-#echo -e "\n"
-#echo "La contraseña no puede estar vacía"
-#echo -e "\n"
-#exit 1
 password="ausias38"
-fi
 
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-useradd -m -p $pass -c $nombrec -d /home/$nombre $nombre
+useradd -m -p $pass $nombre
 chage -d 0 $nombre
 
 
 echo "Los datos ingresados son los siguientes:"
 echo "****************************************"
 echo -e "\n"
-echo "Nombre completo: $nombrec"
 echo "Nombre del usuario: $nombre"
-echo "Directorio home: $home"
+echo "Numero: $comment"
+echo "Direccion: $comment2"
 echo "la contraseña es: $password"
 echo "El usuario fue creado satisfactoriamente"
 host=`hostname`
 echo "El host desde el que se ha creado es: $host"
+echo ${?}
+
